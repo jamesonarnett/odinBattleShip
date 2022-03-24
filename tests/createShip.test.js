5c4dda5c90d6f0b4d21f5ffca2fdb757bb3aa463
Jameson <jamesonarnett@yahoo.com>
2022-03-24 17:32:14 -0500

    Mod scripts && createShip.test

--- tests/createShip.test.js
+++ tests/createShip.test.js
@@ -1,1 +1,5 @@
-//placeholder
+const createShip = require("../src/modules/factories");
+
+it("checks length of ship", () => {
+  expect(new createShip("Cruiser", 4)).toEqual({ name: "Cruiser", length: 4 });
+});
