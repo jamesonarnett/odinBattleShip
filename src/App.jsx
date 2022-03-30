f91e9a0fd46f57ad23e7057cd53725a7429ff2b1
Jameson <jamesonarnett@yahoo.com>
2022-03-30 17:26:56 -0500

    Updates GameArea UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Header from "./components/Header";
 import TextBox from "./components/TextBox";
+import GameArea from "./components/GameArea";
 import Footer from "./components/Footer";
 // eslint-disable-next-line
 import styles from "./scss/App.module.scss";
-import GameArea from "./components/GameArea";
 
 const App = () => {
   return (
--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -3,8 +3,15 @@ import styles from "../scss/GameArea.module.scss";
 const GameArea = () => {
   return (
     <div className={styles.gameArea}>
-      <div className={styles.gameBoardOne}>asdasdasd</div>
-      <div className={styles.gameBoardTwo}>asdasdasd</div>
+      <div className={styles.titleBox}>
+        <h2>FRIENDLY WATERS</h2>
+        <div className={styles.gameBoardOne}></div>
+      </div>
+
+      <div className={styles.titleBox}>
+        <h2>ENEMY WATERS</h2>
+        <div className={styles.gameBoardTwo}></div>
+      </div>
     </div>
   );
 };
