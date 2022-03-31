a61bc435314d23b457d50fc6b71536b7f2ffddf8
Jameson <jamesonarnett@yahoo.com>
2022-03-30 19:23:16 -0500

    Adds Ship types && Icons

---
+++ src/modules/gameHelpers/shipTypes.js
@@ -0,0 +1,15 @@
+const carrier = "../../assets/ships/carrier.png";
+const battleship = "../../assets/ships battleship.png";
+const cruiser = "../../assets/ships cruiser.png";
+const submarine = "../../assets/ships submarine.png";
+const destroyer = "../../assets/ships destroyer.png";
+
+const shipTypes = [
+  { name: "Carrier", length: 5 },
+  { name: "Battleship", length: 4 },
+  { name: "Cruiser", length: 3 },
+  { name: "Submarine", length: 3 },
+  { name: "Destroyer", length: 2 },
+];
+
+module.exports = shipTypes;
