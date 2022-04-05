8266e174058fdd5f3e73030adfbcdf208aef4d62
Jameson <jamesonarnett@yahoo.com>
2022-04-05 16:34:56 -0500

    Change startScreen back to functional component // preferred

--- src/components/gameWindow/StartScreen.jsx
+++ src/components/gameWindow/StartScreen.jsx
@@ -1,17 +1,46 @@
+import { useState } from "react";
 import styles from "../../scss/StartScreen.module.scss";
 
 const StartScreen = () => {
+  const [name, setName] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setName(name.trim());
+    console.log(name);
+
+    if (!name.trim()) {
+      throw new Error("Name required");
+    }
+
+    if (name.trim().length > 15) {
+      throw new Error("Name too long");
+    }
+    //more to come here setError state?
+  };
+
   return (
-    <div className={styles.container}>
+    <form onSubmit={handleSubmit} className={styles.container}>
       <label htmlFor="name">Enter Player Name</label>
-      <input id="name" type={"text"} placeholder="Combatant" />
+      <input
+        onChange={handleName}
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Combatant"
+      />
       <button
         data-back="NOW"
         data-front="PLAY"
         className={styles.btnFlip}
         type=""
       ></button>
-    </div>
+    </form>
   );
 };
 
